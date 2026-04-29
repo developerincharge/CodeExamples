e02ff8b75af95c44cc5180e6c2c83188963fedb3
sabur786 <syedrizvidev007@gmail.com>
2026-04-28 19:59:08 -0500

    added java class

---
+++ src/string_coding/FindTotalSalaryFromDept.java
@@ -0,0 +1,37 @@
+package string_coding;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindTotalSalaryFromDept {
+
+    public static void main(String[] args) {
+        System.out.println("Filter Employee");
+
+        List<Employee> employees = Arrays.asList(
+                new Employee(1, "Alice", "New York", "HR", 60000),
+                new Employee(2, "Bob", "Los Angeles", "IT", 75000),
+                new Employee(3, "Charlie", "Chicago", "Finance", 80000),
+                new Employee(4, "David", "Houston", "IT", 72000),
+                new Employee(5, "Eve", "Phoenix", "HR", 65000),
+                new Employee(6, "Frank", "Philadelphia", "Finance", 95000),
+                new Employee(7, "Grace", "San Antonio", "IT", 110000),
+                new Employee(8, "Hannah", "San Diego", "HR", 55000)
+        );
+
+        double EmpTotalSalary = employees.stream()
+                .filter(emp -> emp.getDepartment().equals("Finance"))
+                .mapToDouble(Employee::getSalary)
+                .sum();
+        System.out.println("Total Salary of Department: " + EmpTotalSalary);
+
+            double totalSum = ((Double) employees.stream()
+                    .filter(emp -> emp.getDepartment().equals("Finance"))
+                            .collect(Collectors.summingDouble(Employee::getSalary)));
+        System.out.println("Total Salary of Department: " + totalSum);
+
+
+
+    }
+}
